47501f758ebbddd47833efcf4812b93b57c5c12e
Gungun <gungun@Gunguns-MacBook-Pro.local>
2019-09-07 13:28:00 +0530

    adding calc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myapp';
+  no1:number=0
+  no2:number=0
+  result:number=0;
+  add(no1,no2)
+  {
+    this.no1=+no1
+    this.no2=+no2
+    this.result=this.no1+this.no2
+    console.log(this.result)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { GreetingComponent } from './greeting/greeting.component';
+import { MyComponent } from './my/my.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    GreetingComponent
+    GreetingComponent,
+    MyComponent
   ],
   imports: [
     BrowserModule,
